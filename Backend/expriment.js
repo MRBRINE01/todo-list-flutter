ac398c879b1e39ce3d6bdd751923c21a9ff81d74
Mayank Agrawal <116949442+MRBRINE01@users.noreply.github.com>
2025-03-01 23:58:13 +0530

    Api for creating new list

--- Backend/expriment.js
+++ Backend/expriment.js
@@ -14,37 +14,54 @@ mongoose.connect(mongoURI).then(()=>{
 });
 
 
-var  listName = "trip";
+// var  listName = "trip";
 
-const taskSchema = new mongoose.Schema({
-    _id: Number,  
-    [listName]: [
-      {
-        id: Number, 
-        task: String,
-        status: {type: Boolean, default: false},
-        date: {type: Date, default: Date.now}
-      }
-    ]
+// const taskSchema = new mongoose.Schema({
+//     _id: Number,  
+//     [listName]: [
+//       {
+//         id: Number, 
+//         task: String,
+//         status: {type: Boolean, default: false},
+//         date: {type: Date, default: Date.now}
+//       }
+//     ]
+// });
+
+const taskListSchema = new mongoose.Schema({
+  listId: Number,  
+  listName: String, 
+  tasks: [{}]
 });
 
-
  
-const Task = mongoose.model("Tasks", taskSchema);
+// const Task = mongoose.model("Tasks", taskSchema);
+const Todo_list = mongoose.model("Todo_list", taskListSchema);
 
+    app.post('/newList', async (req, res)=> {
+      try {
+        
+        console.log("request body: ", req.body);
 
-// const listSchema = new mongoose.Schema({
-//     [listName]: [taskSchema] 
-//   });
+        const {listName} = req.body;
 
-//   const listModel = mongoose.model(listName , listSchema);
+        if(!listName){
+          return res.status(400).json({error: "List name is required"});
+        }
 
-Task.create({
-    _id : 2,
-    [listName]: [
-      { id: 4, task: "brush"},
-      { id: 5, task: "pen"},
-      { id: 6, task: "coffee"}
-    ]
-  }).then(() => console.log("Homework added"))
-    .catch(err => console.error(err));
+        const lastList = await Todo_list.findOne().sort({listId: -1});
+        const newListId = lastList ? lastList.listId + 1 : 1;
+
+        const newList = new Todo_list({ listId: newListId, listName, tasks: [] });
+        const savedTask = await newList.save();
+
+        res.status(201).json({ message: "Task created successfully", task: savedTask });
+      } catch (error) {
+          res.status(500).json({ error: "Error creating task", details: error.message });
+      }
+    });
+
+    // Start Server
+app.listen(PORT,()=>{
+  console.log("Server running on http://localhost:", PORT);
+});
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # todo-list-flutter
+
+
+npm install express mongoose cors body-parser dotenv
+nodemon server.js / node server.js
