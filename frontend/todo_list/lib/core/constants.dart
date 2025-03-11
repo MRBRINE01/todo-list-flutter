166500d3577a0e794651be481c843318860aa236
Ananya Pandit <ananya.pandit.2412@gmail.com>
2025-03-11 22:14:27 +0530

    right panel done with constant colors and added one dependency for datepicker

--- frontend/todo_list/lib/core/constants.dart
+++ frontend/todo_list/lib/core/constants.dart
@@ -5,6 +5,12 @@ class Constants {
 
   static Color nonHoverColor = Color.fromARGB(255, 43, 43, 43);
   static Color hoverColor = Color.fromARGB(255, 63, 63, 63);
-
   static Color surfaceColor = Color.fromARGB(255, 27, 27, 27);
+  static Color textColor = Colors.white;
+  static Color infoContainerColor = Color.fromARGB(255, 43, 43, 43);
+  static Color hintTextColor = Color.fromARGB(255, 149, 149, 149);
+  static Color dividerColor = Color.fromARGB(255, 81, 81, 81);
+  static Color calender1Color =  Color.fromARGB(255, 223, 223, 223);
+  static Color calender2Color = Color.fromARGB(255, 160, 160, 160);
+  static Color calender3Color = Color.fromARGB(255, 181, 181, 181);
 }
--- frontend/todo_list/lib/views/right_panel.dart
+++ frontend/todo_list/lib/views/right_panel.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:todo_list/core/constants.dart';
 
 class RightPanel extends StatefulWidget {
   const RightPanel({super.key});
@@ -9,96 +11,124 @@ class RightPanel extends StatefulWidget {
 
 class _RightPanelState extends State<RightPanel> {
   bool isChecked = false;
- 
+
+  TextEditingController dateController = TextEditingController();
+  String? selectedDate;
+  String? apiDate = DateFormat('yyyy-MM-dd').format(DateTime.now());
+
+  Future<void> selectDate(BuildContext context) async {
+    DateTime? pickedDate = await showDatePicker(
+        context: context,
+        firstDate: DateTime(2000),
+        lastDate: DateTime(2100),
+        builder: (BuildContext context, Widget? child) {
+          return Theme(
+              data: ThemeData(
+                  colorScheme: ColorScheme.dark(
+                      primary: Constants.calender1Color,
+                      onPrimary: Constants.calender1Color,
+                      onSurface: Constants.calender1Color)
+                      ),
+              child: child!);
+        });
+
+    if (pickedDate != null) {
+      setState(() {
+        selectedDate = DateFormat('dd-MM-yyyy').format(pickedDate);
+
+        print("Selected Date: $selectedDate");
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.only(left: 20, right: 20, top: 40, bottom: 10),
+            padding:
+                const EdgeInsets.only(left: 20, right: 20, top: 40, bottom: 10),
             child: Container(
-              
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(5),
-                color: Color.fromARGB(255, 43, 43, 43),
+                color: Constants.infoContainerColor,
               ),
-                
               height: 60,
-              
               child: Row(
                 children: [
                   Padding(
                     padding: const EdgeInsets.all(10),
                     child: Checkbox(
-                        value: isChecked,
-                        onChanged: (bool? value) {
-                          setState(() {
-                            isChecked = value!;
-                          });
-                        },
-                        checkColor: Colors.white,
-                        side: BorderSide(color: Colors.white, width: 1),
-
-                        ),
+                      value: isChecked,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          isChecked = value!;
+                        });
+                      },
+                      checkColor: Constants.textColor,
+                      side: BorderSide(color: Constants.textColor, width: 1),
+                    ),
                   ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 5),
-                        child: Text("Task name",style: TextStyle(color: Colors.white, 
+                  Padding(
+                    padding: const EdgeInsets.only(left: 5),
+                    child: Text(
+                      "Task name",
+                      style: TextStyle(
+                        color: Constants.textColor,
                         fontSize: 20,
-                        decoration: isChecked ? TextDecoration.lineThrough : TextDecoration.none,
-                        decorationColor: Colors.white, 
+                        decoration: isChecked
+                            ? TextDecoration.lineThrough
+                            : TextDecoration.none,
+                        decorationColor: Constants.textColor,
                         decorationThickness: 2,
-                          
-                        ),
-                        
-                        ),
                       ),
+                    ),
+                  ),
                 ],
               ),
             ),
           ),
-
-
           Padding(
-            padding: const EdgeInsets.only(left: 20, right: 20, top: 10, bottom: 10),
+            padding:
+                const EdgeInsets.only(left: 20, right: 20, top: 10, bottom: 10),
             child: Container(
-              
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(5),
-                color: Color.fromARGB(255, 43, 43, 43),
+                color: Constants.infoContainerColor,
               ),
-                
               height: 60,
-              
               child: Row(
                 children: [
                   Padding(
                     padding: const EdgeInsets.all(10),
-                    child: Icon(Icons.wb_sunny_outlined, size: 25, color: Colors.white,),
+                    child: Icon(
+                      Icons.wb_sunny_outlined,
+                      size: 25,
+                      color: Constants.textColor,
+                    ),
                   ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 10),
-                        child: Text("Add to my day",style: TextStyle(color: Colors.white, fontSize: 20),
-                        ),
-                      ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: Text(
+                      "Add to my day",
+                      style:
+                          TextStyle(color: Constants.textColor, fontSize: 20),
+                    ),
+                  ),
                 ],
               ),
             ),
           ),
-
-
-
           Padding(
-            padding: const EdgeInsets.only(left: 20, right: 20, top: 10, bottom: 10),
+            padding:
+                const EdgeInsets.only(left: 20, right: 20, top: 10, bottom: 10),
             child: Container(
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(5),
-                color: Color.fromARGB(255, 43, 43, 43),
+                color: Constants.infoContainerColor,
               ),
-                
               height: 130,
-              
               child: Column(
                 children: [
                   Padding(
@@ -107,69 +137,87 @@ class _RightPanelState extends State<RightPanel> {
                       children: [
                         Padding(
                           padding: const EdgeInsets.all(10),
-                          child: Icon(Icons.watch_later_outlined, color: Colors.white,),
+                          child: Icon(
+                            Icons.watch_later_outlined,
+                            color: Constants.textColor,
+                          ),
                         ),
                         Padding(
                           padding: const EdgeInsets.only(left: 10),
-                          child: Text("Remind Me", style: TextStyle(color: Colors.white, fontSize: 20),
+                          child: Text(
+                            "Remind Me",
+                            style: TextStyle(
+                                color: Constants.textColor, fontSize: 20),
                           ),
                         )
                       ],
                     ),
                   ),
                   Padding(
-                    padding: const EdgeInsets.only(right: 10,left: 10),
-                    child: Divider(thickness: 1, color: const Color.fromARGB(255, 81, 81, 81)
-                    ),
+                    padding: const EdgeInsets.only(right: 10, left: 10),
+                    child: Divider(thickness: 1, color: Constants.dividerColor),
                   ),
-                  Row(
-                    children: [
-                      Padding(
-                    padding: const EdgeInsets.only(top: 10),
+                  GestureDetector(
+                    onTap: () => selectDate(context),
                     child: Row(
                       children: [
                         Padding(
-                          padding: const EdgeInsets.all(10),
-                          child: Icon(Icons.calendar_month, color: Colors.white,),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 10),
-                          child: Text("Add Due Date", style: TextStyle(color: Colors.white, fontSize: 20),
+                          padding: const EdgeInsets.only(top: 10),
+                          child: Row(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(10),
+                                child: Icon(
+                                  Icons.calendar_month,
+                                  color: Constants.textColor,
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 10),
+                                child: Text(
+                                  selectedDate ?? "Add Due Date",
+                                  style: TextStyle(
+                                      color: Constants.textColor, fontSize: 20),
+                                ),
+                              )
+                            ],
                           ),
-                        )
-                    
+                        ),
                       ],
                     ),
-                  ),
-                    ],
                   )
                 ],
               ),
-
-              
             ),
           ),
-
           Padding(
-          padding: EdgeInsets.all(16.0),
-          child: TextField(
-            cursorColor: Colors.white,
-            decoration: InputDecoration(
-              hintText: "Add Note",
-              hintStyle: TextStyle(color: const Color.fromARGB(255, 149, 149, 149)
+            padding: EdgeInsets.all(16.0),
+            child: TextField(
+              cursorColor: Constants.textColor,
+              decoration: InputDecoration(
+                hintText: "Add Note",
+                hintStyle: TextStyle(color: Constants.hintTextColor),
+                filled: true,
+                fillColor: Constants.infoContainerColor,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5),
+                  borderSide: BorderSide.none,
+                ),
               ),
-              filled: true,
-              fillColor: Color.fromARGB(255, 43, 43, 43), 
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(5),
-                borderSide: BorderSide.none, 
-              ),
+              maxLines: 5,
+              keyboardType: TextInputType.multiline,
+              style: TextStyle(fontSize: 16, color: Constants.textColor),
             ),
-            maxLines: 5, 
-            keyboardType: TextInputType.multiline,
-            style: TextStyle(fontSize: 16, color: Colors.white), 
           ),
+          Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Created on 2-04-2024",
+                  style: TextStyle(color: Constants.textColor)),
+            ],
           ),
+          SizedBox(height: 15),
         ],
       ),
     );
